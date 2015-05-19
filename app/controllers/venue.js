c7f10681315579313c6af6d1082e9c2c5b5470a9
Regina Peyfuss <regina.peyfuss@state.co.us>
2015-05-19 13:36:06 -0600

    conversation tab

--- app/controllers/venue.js
+++ app/controllers/venue.js
@@ -1,1 +1,20 @@
 var args = arguments[0] || {};
+$.maptext.text = Alloy.CFG.venueAddress;
+$.phonetext.text = Alloy.CFG.venuePhoneNumber;
+
+function clickmap(evt) {
+	Ti.Platform.canOpenURL('https://maps.google.com/maps?q=' + encodeURIComponent(Alloy.CFG.venueAddress));
+	
+}
+
+function clickcall(evt){
+	if (OS_IOS){
+		if (Ti.Platform.canOpenURL('tel://' + Alloy.CFG.venuePhoneNumber)){
+			Ti.Platform.canOpenURL('tel://' + Alloy.CFG.venuePhoneNumber);
+		}else{
+			alert(L('no_phone'));
+		}
+	}else {
+		Ti.Platform.canOpenURL('tel://' + Alloy.CFG.venuePhoneNumber);
+	}
+}
